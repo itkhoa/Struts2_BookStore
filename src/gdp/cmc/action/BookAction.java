9c511a5bf8004b64f413db66f56d3e118f89e924
khoanv580 <khoanv_58@vnu.edu.vn>
2017-08-29 09:22:51 +0700

    update detail

--- src/gdp/cmc/action/BookAction.java
+++ src/gdp/cmc/action/BookAction.java
@@ -7,12 +7,28 @@ import gdp.cmc.bus.BookBus;
 import gdp.cmc.model.*;
 public class BookAction {
   private List<Book> list;
+  private Book book;
+  private String id;
 
   public List<Book> getList() {
     return list;
   }
   public void setList(List<Book> list) {
     this.list = list;
+  }
+  
+  public Book getBook() {
+    return book;
+  }
+  public void setBook(Book book) {
+    this.book = book;
+  }
+  
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
   }
   public String execute() {
     return "success";
@@ -21,6 +37,13 @@ public class BookAction {
     BookBus bookBus = new BookBus();
     this.list = bookBus.getAllBook();
     this.list.get(1).getAuthorName();
+    return "success";
+  }
+  
+  public String getBookById() throws NumberFormatException, ClassNotFoundException, SQLException {
+    BookBus bookBus = new BookBus();
+    System.out.println(id);
+    book = bookBus.getBookById(Integer.parseInt(id));
     return "success";
   }
 }
